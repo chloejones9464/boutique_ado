404eefdc9808166b0e2928af75a9c39d962e527d
chloejones9464 <jones.chloe@live.co.uk>
2025-08-29 20:04:20 +0100

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,10 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from products.models import Product
 
-# Create your models here.
+from products.models import Product
 
 
 class Order(models.Model):
@@ -22,13 +22,18 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
     def _generate_order_number(self):
-        """ Generate a random, unique order number using UUID """
+        """
+        Generate a random, unique order number using UUID
+        """
         return uuid.uuid4().hex.upper()
     
     def update_total(self):
-        """ Update grand total each time a line item is added,
+        """
+        Update grand total each time a line item is added,
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
@@ -40,13 +45,14 @@ class Order(models.Model):
         self.save()
     
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number
+        """
+        Override the original save method to set the order number
         if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-        
+
     def __str__(self):
         return self.order_number
 
@@ -57,10 +63,11 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-    
+
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number
-        if it hasn't been set already.
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
